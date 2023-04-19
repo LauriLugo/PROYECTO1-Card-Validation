64cdbcd9a055e10d1aab0ad7162cba2797922c83
Laura Lugo <lauri.lugo7@gmail.com>
2023-04-19 16:00:14 -0500

    JS funcionando

--- src/index.js
+++ src/index.js
@@ -2,12 +2,46 @@ import validator from './validator.js';
 
 // console.log(validator);
 
-validator.isValid()
-validator.maskify()
+// validator.isValid()
+// validator.maskify()
+const entradaTarjeta = document.getElementById("numberCard");
+entradaTarjeta.addEventListener("input", validarYEnmascarar);
 
-var texto = document.getElementById("numberCard");
-texto.value = "green";
+const botonAceptar = document.getElementById("aceptar")
+botonAceptar.addEventListener("click", validarNumero)
 
-function mascara() {
-    document.getElementById("numberCard").value = "1231232432435"
+let numeroTarjetaInterno = "";
+
+function validarYEnmascarar(evento) {
+  if (evento.inputType === "insertText") {
+    // console.log("miau")
+    // console.log(evento.data)
+    numeroTarjetaInterno = numeroTarjetaInterno + evento.data;
+    // console.log(numeroTarjetaInterno)
+  }
+  if (evento.inputType === "deleteContentBackward") {
+    // console.log("prrrr")
+    numeroTarjetaInterno = numeroTarjetaInterno.substring(0, numeroTarjetaInterno.length - 1)
+    // console.log(numeroTarjetaInterno)
+  }
+  if (evento.inputType === "insertFromPaste") {
+    // console.log("kijj")
+    numeroTarjetaInterno = entradaTarjeta.value
+    // console.log(numeroTarjetaInterno)
+  }
+
+  entradaTarjeta.value = validator.maskify(numeroTarjetaInterno)
+  // console.log("hola mundo")
+  // console.log(evento)
 }
+
+function validarNumero(evento) {
+  evento.preventDefault()
+  // console.log("algo")
+  if (validator.isValid(numeroTarjetaInterno)) {
+    alert("La tarjeta es válida")
+  } else {
+    alert("La tarjeta no es válida")
+  }
+}
+
